bb535dfb9494588b1dac371c0038dd09c6c44c1b
Mksricard <34170972+Dragonfrut@users.noreply.github.com>
2020-04-08 16:14:54 -0700

    character change
    animations were not working correctly and could not figure out how to fix. Changed character direction to a monkey with no animations

---
+++ JumpGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform target;
+
+    private bool followPlayer;
+
+    public float min_Y_Threshold = -2.6f;
+
+    // Start is called before the first frame update
+    void Awake() {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Follow();
+    }
+
+    void Follow() {
+
+        //if player goes below camera, do not follow
+        if (target.position.y < (transform.position.y - min_Y_Threshold)) {
+            followPlayer = false;
+        }
+
+        //if player goes above camera, follow
+        if (target.position.y > transform.position.y) {
+            followPlayer = true;
+        }
+
+       if (followPlayer) {
+            Vector3 temp = transform.position;
+            temp.y = target.position.y;
+            transform.position = temp;
+        }
+            
+    
+    }
+
+
+}//class
