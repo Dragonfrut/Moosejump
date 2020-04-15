3134282c3c29413a3003c46a35a01e914e07299c
Mksricard <34170972+Dragonfrut@users.noreply.github.com>
2020-04-15 11:29:02 -0700

    rebuild game for second time
    finished base gameplay loop, made main menu, and score system. still need end game screen

---
+++ Jump game/Assets/Scripts/Ememy Script/EnemyScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    //making variables for current position and edge positions
+    private SpriteRenderer mySpriteRenderer;
+    private Vector3 myPosition;
+    private Vector3 pos1;
+    private Vector3 pos2;
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+     void Awake() {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+
+    }//Awake 
+
+    // Update is called once per frame
+    void Update() {
+
+        myPosition = transform.position;
+
+        pos1 = new Vector3(-2.634f, myPosition.y, myPosition.z);
+        pos2 = new Vector3(2.634f, myPosition.y, myPosition.z);
+
+        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
+
+        //trying to flip enemy sprite not working
+        /*if (myPosition.x == pos1.x) {
+            if (mySpriteRenderer.flipX == true) {
+                mySpriteRenderer.flipX = false;
+            } else {
+                mySpriteRenderer.flipX = true;
+            }
+        } else if (myPosition.x == pos2.x) {
+            if (mySpriteRenderer.flipX == true) {
+                mySpriteRenderer.flipX = false;
+            } else {
+                mySpriteRenderer.flipX = true;
+            }
+        }*/
+    }//update
+
+
+}//class
--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Jump game
 A 2D platform jumper with tilt controls
  
+https://answers.unity.com/questions/952558/how-to-flip-sprite-horizontally-in-unity-2d.html
+
+https://answers.unity.com/questions/690884/how-to-move-an-object-along-x-axis-between-two-poi.html
+
+https://docs.unity3d.com/ScriptReference/SpriteRenderer.html
+
+https://docs.unity3d.com/ScriptReference/SpriteRenderer-flipX.html
+
+https://docs.unity3d.com/ScriptReference/Vector3.Lerp.html
+
+https://www.youtube.com/watch?v=QbqnDbexrCw
+
+https://www.youtube.com/watch?time_continue=6&v=zc8ac_qUXQY&feature=emb_logo
+
+https://answers.unity.com/questions/1175961/how-to-use-accelerometer-to-move-object-by-tilting.html
+
+https://www.youtube.com/watch?v=wpSm2O2LIRM
